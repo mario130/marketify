b5c29b9346450a7a8986b3cf0abd77f7baa73968
MostafaFathy367 <mostafa.fathy367@gmail.com>
2021-03-12 13:36:03 +0200

    Add Home Page

--- marketify/src/app/pages/homepage/homepage.component.ts
+++ marketify/src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  productsComponents = [
+  {img:'../../../assets/images/image-component-table.webp',name:'Tables'},
+  {img:'../../../assets/images/component-credenzas.webp',name:'Credenzas'},
+  {img:'../../../assets/images/component-shelves.webp',name:'Shelves'},
+  {img:'../../../assets/images/component-rugs-3.webp',name:'Rugs'},
+  {img:'../../../assets/images/component-accessories-2.webp',name:'Accessories'}
+];
+showMore = false;
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  next() {
+    this.showMore = true;
+  }
+  prev() {
+    this.showMore = false;
+  }
+  goToComponent(category){
+    this.router.navigate(['/products',category]);
+  }
 }
